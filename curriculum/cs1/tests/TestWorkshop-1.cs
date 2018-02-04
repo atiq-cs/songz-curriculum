cdb74be2fece90b7da51e9e7db8d6a3d6c80143f
Atiq Rahman <atiqcs@outlook.com>
2018-02-03 22:05:22 -0800

    Initial environment setup for C# (Visual Studio Minimal) Source Project and Unit Testing Project

---
+++ curriculum/cs1/tests/TestWorkshop-1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using solutions;
+
+namespace tests {
+  [TestClass]
+  public class TestWorkshop1 {
+    [TestMethod]
+    public void TestAdd() {
+    }
+  }
+}
