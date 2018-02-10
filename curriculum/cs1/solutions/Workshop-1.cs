5d2339e7c7b2e410eb916cf2ebb1286ea18e52ca
Atiq Rahman <atiqcs@outlook.com>
2018-02-10 15:30:54 -0800

    Problem#1 and test

--- curriculum/cs1/solutions/Workshop-1.cs
+++ curriculum/cs1/solutions/Workshop-1.cs
@@ -2,5 +2,12 @@
 
 namespace solutions {
   public class Workshop1 {
+    /*
+     * 1.1. (sum2)
+     * takes in 2 numbers, and returns their sum.
+     */
+    public int Add(int a, int b) {
+      return a + b;
+    }
   }
 }
--- curriculum/cs1/tests/TestWorkshop-1.cs
+++ curriculum/cs1/tests/TestWorkshop-1.cs
@@ -8,6 +8,20 @@ namespace tests {
   public class TestWorkshop1 {
     [TestMethod]
     public void TestAdd() {
+      // Test against a list of values
+      List<int> inputValuesArg1 = new List<int> { 1, 2, 3, 5 };
+      List<int> inputValuesArg2 = new List<int> { 6, 7, 8, 9 };
+      List<int> expectedValues  = new List<int> { 7, 9, 11, 14 };
+
+      Workshop1 workshop = new Workshop1();
+
+      for (int i = 0; i < inputValuesArg1.Count; i++) {
+        int input1 = inputValuesArg1[i];
+        int input2 = inputValuesArg2[i];
+        int expected = expectedValues[i];
+        int actual = workshop.Add(input1, input2);
+        Assert.AreEqual(expected, actual, "func Add failed for " + input1 + ", " + input2);
+      }
     }
   }
 }
